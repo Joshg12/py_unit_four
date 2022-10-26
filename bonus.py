43eca68f6370fb6afe423b8df1f16c65484f448c
Josh <goldsjo@ssfs.org>
2022-10-26 08:44:18 -0400

    Commit

---
+++ bonus.py
@@ -0,0 +1,19 @@
+#Joshua G
+
+
+def salarycalc(longevity, current):
+    if longevity >5:
+        salary = current
+    else:
+        salary = current
+    return salary
+
+def main():
+    longevity = int(input("How long have you been working here?"))
+    current = int(input("What is your current salary?"))
+    final = salarycalc(longevity, current)
+    print("Your salary is:", int(round(final)))
+
+
+main()
+
