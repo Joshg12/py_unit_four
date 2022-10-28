b169f47bc23b44a56881c367e9ac3c80bd830b78
Josh <goldsjo@ssfs.org>
2022-10-28 13:56:41 -0400

    Commit

--- bonus.py
+++ bonus.py
@@ -17,3 +17,15 @@ def main():
 
 main()
 
+
+
+import random
+
+def main():
+
+    nunber = random,randint(1,10)
+    print(number)
+
+
+    if number %
+
---
+++ maximum.py
@@ -0,0 +1,15 @@
+
+
+
+import random
+
+
+def main():
+    nunber = random, randint(1, 10)
+    print(number)
+
+    if number %
+
+
+
+
