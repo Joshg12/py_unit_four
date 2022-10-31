d7da5cb8a4782e904e5dbdcd99e4ebe4426e57a2
Josh <goldsjo@ssfs.org>
2022-10-31 15:09:40 -0400

    Commit

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,11 +1,24 @@
+import random
 def who_wins(user, computer):
     # 1 is rock, 2 is paper, 3 is scissors
+    if computer == user:
+        return "Tie"
+    elif computer == 1 and user == 2:
+        return "You win!"
+    elif computer == 2 and user == 1:
+        return "Computer wins."
+    elif computer == 1 and user == 3
+        return""
     pass
 
 
 def main():
-    pass
+    input("Enter 1 for rock, 2 for paper, and 3 for scissors.")
+
+
 
 
 if __name__ == '__main__':
     main()
+
+
--- triangles.py
+++ triangles.py
@@ -1,10 +1,12 @@
 def is_triangle(side1, side2, side3):
-    pass
+    if side1 + side2 <= side3 or side1 + side3 <= side2 or side2+ side3 <= side1:
+        return "These legnths do not make a triangle."
+
 
 
 
 def main():
-    pass
+
 
 
 if __name__ == '__main__':
