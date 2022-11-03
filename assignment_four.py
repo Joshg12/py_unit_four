53c721b2f054a3a639d895c898e79f1f88b6393b
Josh <goldsjo@ssfs.org>
2022-11-03 11:35:52 -0400

    Commit

---
+++ assignment_four.py
@@ -0,0 +1,39 @@
+import random
+
+
+def cardchoice():
+    input("Woukd you like another card? Type Y for yes, type N for no.")
+    if ("Y"):
+        return getcard()
+    else:
+        return "N"
+
+def getcard():
+    x = random.randint(1, 10)
+    print("You drew",x)
+    return x
+def usertotal():
+    usertotal = getcard()
+
+def who_wins(user, dealer):
+
+    if dealer >= 21 and user >= 21:
+        return "Both lose."
+    elif dealer == user:
+        return "Tie."
+    elif dealer >= 21:
+        return "You win!"
+    elif user >= 21:
+        return "Dealer wins."
+    elif dealer >= user:
+        return "Dealer wins."
+    elif user >= dealer:
+        return"You win!"
+
+
+'''
+This function will tell us who
+if wins:
+:param User: The user's card total
+:param Dealer: The dealer's card total
+'''
